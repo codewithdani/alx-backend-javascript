b956113b9325998744b416e78f7465cda51798ee
Daniel <daniel@gmail.com>
2023-11-27 18:06:43 +0300

    2-arrow.js

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    env: {
-      node: true,
-      jest: true,
-    },
-    extends: ['airbnb-base'],
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-    },
-    rules: {
-      'linebreak-style': 'off',
-      'no-console': 'off',
-      'import/extensions': 'off',
-      'import/no-unresolved': 'off',
-    },
-  };
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'no-console': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+  },
+};
