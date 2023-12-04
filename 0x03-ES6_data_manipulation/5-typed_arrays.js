ba49f4463f4d693145159e9354fcf9e3738887ee
Daniel <daniel@gmail.com>
2023-12-04 20:48:16 +0300

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -7,7 +7,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
 
   // Create an Int8Array view on the ArrayBuffer
-  const int8Array = new Int8Array(buffer);
+  const int8Array = new Int8Array(buffer, 0, length);
 
   // Set the value at the specified position
   int8Array[position] = value;
