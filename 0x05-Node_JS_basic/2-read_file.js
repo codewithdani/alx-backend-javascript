96e035df82a0a55a2627af1ce128c14806e5d206
Daniel <daniel@gmail.com>
2024-01-30 17:42:05 +0300

    package.json

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Daniel Giday <https://github.com/codewithdani>
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
